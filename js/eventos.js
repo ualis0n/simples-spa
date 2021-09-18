c787fa58c61546337b880fd3ee33a165a1ab4556
ualis0n <gabriel_fontenelle_@hotmail.com>
2021-09-18 10:44:47 -0300

    remover ou adicionar cor no botão

--- js/eventos.js
+++ js/eventos.js
@@ -1,14 +1,26 @@
 const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
 const BTN_LISTAR = document.getElementById('btn-listar');
-const BTN_CADASTRAR = document.getElementsByClassName('section-cadastrar');
-const BTN_LISTAR = document.getElementById('section-listar')
+const SECTION_CADASTRAR = document.getElementById('section-cadastrar');
+const SECTION_LISTAR = document.getElementById('section-listar');
 
-BTN_LISTAR.addEventListener('click', function (){
-  SECTION_CADASTRO.style.display = 'none';
+BTN_LISTAR.addEventListener('click', function () {
+  SECTION_CADASTRAR.style.display = 'none';
   SECTION_LISTAR.style.display = 'block';
+
+  BTN_LISTAR.classList.remove('btn-outline-dark');
+  BTN_LISTAR.classList.add('btn-dark');
+  BTN_CADASTRAR.classList.remove('btn-dark');
+  BTN_CADASTRAR.classList.add('btn-outline-dark');
+
 });
 
 BTN_CADASTRAR.addEventListener('click', function () {
   SECTION_LISTAR.style.display = 'none';
-  SECTION_CADASTRO.style.display = 'block';
+  SECTION_CADASTRAR.style.display = 'block';
+
+  BTN_LISTAR.classList.add('btn-outline-dark');
+  BTN_LISTAR.classList.remove('btn-dark');
+  BTN_CADASTRAR.classList.add('btn-dark');
+  BTN_CADASTRAR.classList.remove('btn-outline-dark');
+
 });
