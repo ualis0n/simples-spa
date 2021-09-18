57a4695c7ea3b013e3e125d616e3f1be37ffba2c
ualis0n <gabriel_fontenelle_@hotmail.com>
2021-09-18 09:52:30 -0300

    creie funcoes para os botoes usando JS

---
+++ js/eventos.js
@@ -0,0 +1,4 @@
+const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
+const BTN_LISTAR = document.getElementById('btn-listar');
+const BTN_CADASTRAR = document.getElementsByClassName('section-cadastrar');
+const BTN_LISTAR = document.getElementById('section-listar')
