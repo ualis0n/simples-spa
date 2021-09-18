a72691ffbf2dbb6a0c43a58dd010983c61026dcd
ualis0n <gabriel_fontenelle_@hotmail.com>
2021-09-18 10:16:29 -0300

    atibuição no js pra clicar e aparecer e ocultar

--- js/eventos.js
+++ js/eventos.js
@@ -2,3 +2,13 @@ const BTN_CADASTRAR = document.getElementById('btn-cadastrar');
 const BTN_LISTAR = document.getElementById('btn-listar');
 const BTN_CADASTRAR = document.getElementsByClassName('section-cadastrar');
 const BTN_LISTAR = document.getElementById('section-listar')
+
+BTN_LISTAR.addEventListener('click', function (){
+  SECTION_CADASTRO.style.display = 'none';
+  SECTION_LISTAR.style.display = 'block';
+});
+
+BTN_CADASTRAR.addEventListener('click', function () {
+  SECTION_LISTAR.style.display = 'none';
+  SECTION_CADASTRO.style.display = 'block';
+});
